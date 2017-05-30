22f93b06ab9abfa5a59e129aa8a16e51fcb3ef1d
Danielle Fricke <danielle.fricke11@gmail.com>
2017-05-30 17:51:19 -0400

    views all started

--- app.rb
+++ app.rb
@@ -5,3 +5,26 @@ require 'active_record'
 require_relative 'db/connection'
 
 require_relative 'models/pokemon'
+
+get '/pokemon' do
+  erb :index
+end
+
+get '/pokemon/new' do
+    erb :"pokemon/new"
+end
+
+get 'pokemon/pokemons' do
+  @pokemon = Pokemon.all
+  erb :"pokemons/pokemons"
+end
+
+get '/pokemon/:id' do
+    @pokemon = Pokemon.find(params[:id])
+    erb :'pokemon/show'
+end
+
+post 'pokemon/pokemons' do
+  @pokemon = Pokemons.create(params[:artist])
+redirect "/pokemon/#{@pokemon.id}"
+end
